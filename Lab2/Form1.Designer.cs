885d8820b2ca0feedd215d22d177b7a3a73b5ea9
Mary Liashenko <ihlavsya@gmail.com>
2017-09-29 18:35:13 +0300

    First steps interface

--- Lab2/Form1.Designer.cs
+++ Lab2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab2
 {
-    partial class Form1
+    partial class PictureEditorForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBoxPalette = new System.Windows.Forms.PictureBox();
+            this.openFileDialogPalette = new System.Windows.Forms.OpenFileDialog();
+            this.buttonGetPalette = new System.Windows.Forms.Button();
+            this.pictureBoxOrigin = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialogOrigin = new System.Windows.Forms.OpenFileDialog();
+            this.buttonGetOrigin = new System.Windows.Forms.Button();
+            //this.buttonChange = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPalette)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrigin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxPalette
+            // 
+            this.pictureBoxPalette.Location = new System.Drawing.Point(12, 65);
+            this.pictureBoxPalette.Name = "pictureBoxPalette";
+            this.pictureBoxPalette.Size = new System.Drawing.Size(362, 440);
+            this.pictureBoxPalette.TabIndex = 0;
+            this.pictureBoxPalette.TabStop = false;
+            // 
+            // openFileDialogPalette
+            // 
+            this.openFileDialogPalette.FileName = "openFileDialogPalette";
+            // 
+            // buttonGetPalette
+            // 
+            this.buttonGetPalette.Location = new System.Drawing.Point(12, 12);
+            this.buttonGetPalette.Name = "buttonGetPalette";
+            this.buttonGetPalette.Size = new System.Drawing.Size(362, 25);
+            this.buttonGetPalette.TabIndex = 1;
+            this.buttonGetPalette.Text = "Open the file *.bmp for picture-palette";
+            this.buttonGetPalette.UseVisualStyleBackColor = true;
+            this.buttonGetPalette.Click += new System.EventHandler(this.buttonGetPicture_Click);
+            // 
+            // pictureBoxOrigin
+            // 
+            this.pictureBoxOrigin.Location = new System.Drawing.Point(411, 65);
+            this.pictureBoxOrigin.Name = "pictureBoxOrigin";
+            this.pictureBoxOrigin.Size = new System.Drawing.Size(361, 440);
+            this.pictureBoxOrigin.TabIndex = 2;
+            this.pictureBoxOrigin.TabStop = false;
+            this.pictureBoxOrigin.Click += new System.EventHandler(this.pictureBox1_Click);
+            //
+            //
+            //
+            
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(809, 65);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(319, 440);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialogOrigin
+            // 
+            this.openFileDialogOrigin.FileName = "openFileDialogOrigin";
+            // 
+            // buttonGetOrigin
+            // 
+            this.buttonGetOrigin.Location = new System.Drawing.Point(411, 13);
+            this.buttonGetOrigin.Name = "buttonGetOrigin";
+            this.buttonGetOrigin.Size = new System.Drawing.Size(361, 24);
+            this.buttonGetOrigin.TabIndex = 4;
+            this.buttonGetOrigin.Text = "Open the file *.bmp for picture-origin ";
+            this.buttonGetOrigin.UseVisualStyleBackColor = true;
+            this.buttonGetOrigin.Click += new System.EventHandler(this.buttonGetOrigin_Click);
+            // 
+            // PictureEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1140, 530);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.buttonGetOrigin);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBoxOrigin);
+            this.Controls.Add(this.buttonGetPalette);
+            this.Controls.Add(this.pictureBoxPalette);
+            this.Name = "PictureEditorForm";
+            this.Text = "PictureEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PictureEditorForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPalette)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrigin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxPalette;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPalette;
+        private System.Windows.Forms.Button buttonGetPalette;
+        private System.Windows.Forms.PictureBox pictureBoxOrigin;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialogOrigin;
+        private System.Windows.Forms.Button buttonGetOrigin;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.Button buttonChange;
     }
 }
 
--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -10,11 +10,76 @@ using System.Windows.Forms;
 
 namespace Lab2
 {
-    public partial class Form1 : Form
+    public partial class PictureEditorForm : Form
     {
-        public Form1()
+        public PictureEditorForm()
         {
             InitializeComponent();
+            openFileDialogPalette.Filter = "BMP files(*.bmp)|*.bmp|All files(*.*)|*.*";
+            openFileDialogOrigin.Filter = "BMP files(*.bmp)|*.bmp|All files(*.*)|*.*";
+        }
+
+        private void buttonGetPicture_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogPalette.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            // получаем выбранный файл
+            string filename = openFileDialogPalette.FileName;
+            pictureBoxPalette.Image = Image.FromFile(filename);
+            pictureBoxPalette.SizeMode=PictureBoxSizeMode.StretchImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            pictureBoxOrigin.Image = Image.FromFile(openFileDialogPalette.FileName);
+            pictureBoxPalette.Image = Image.FromFile(openFileDialogOrigin.FileName);
+        }
+
+        private void buttonGetOrigin_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogOrigin.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            // получаем выбранный файл
+            string filename = openFileDialogOrigin.FileName;
+            pictureBoxOrigin.Image = Image.FromFile(filename);
+            pictureBoxOrigin.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (openFileDialogOrigin.FileName!="" && openFileDialogPalette.FileName!=null)
+            {
+                Label labelSum = new Label();
+                labelSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+                labelSum.Location = new System.Drawing.Point(375, 239);
+                labelSum.Name = "labelSum";
+                labelSum.Size = new System.Drawing.Size(25, 32);
+                labelSum.TabIndex = 5;
+                labelSum.Text = "+";
+                this.Controls.Add(labelSum);
+
+                Button buttonChange = new Button();
+                buttonChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+                buttonChange.Location = new System.Drawing.Point(380, 104);
+                buttonChange.Name = "buttonChange";
+                buttonChange.Size = new System.Drawing.Size(25, 57);
+                buttonChange.TabIndex = 5;
+                buttonChange.Text = "←→\r\n";
+                buttonChange.UseVisualStyleBackColor = true;
+                buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+                this.Controls.Add(buttonChange);
+            }
+        }
+
+        private void PictureEditorForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Controls.Remove(labelSum);
+            this.Controls.Remove(buttonChange);
         }
     }
 }
